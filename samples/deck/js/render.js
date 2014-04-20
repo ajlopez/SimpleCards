eb471ad06729354f7977e518f062c4c89cf6a362
ajlopez <webmaster@ajlopez.com>
2014-04-20 18:44:50 -0300

    Improved renderer deck sample

--- samples/deck/js/render.js
+++ samples/deck/js/render.js
@@ -1,10 +1,24 @@
 var content = $("#content");
 
-function show(card) {
+function show(card, previous) {
     content.empty();
+    
+    if (previous) {
+        var back = $("<button>").html('Back');
+        back.click(function () { show(previous); });
+        content.append(back);
+    }
+    
     var title = $("<h1>").html(card.get('title'));
     title.attr('align', 'center');
     content.append(title);
+    
+    var text = card.get('text');
+    
+    if (text) {
+        var divtext = $("<div>").html(text);
+        content.append(divtext);
+    }
     
     var cards = card.cards();
     
@@ -12,6 +26,7 @@ function show(card) {
         cards.forEach(function (subcard) {
             var button = $("<button>").html(subcard.get('title'));
             button.css("minWidth", '95%');
+            button.click(function () { show(subcard, card); });       
             var div = $("<div>");
             div.append(button);
             div.attr('align', 'center');
